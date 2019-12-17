493560b05d6b1e371ee06e56ccbed180624ac499
Nicolas <nicolas.gauss.1@orange.fr>
2019-12-17 10:26:14 +0100

    Commentaires + corrections pbs

--- Serveur/app.js
+++ Serveur/app.js
@@ -1,25 +1,41 @@
-// Chargement de socket.io
-var io = require("socket.io");
+// Imports modules
 var user_management = require("./user_management");
-const server = io.listen(3000);
+
+var path = require("path");
+var express = require("express");
+var app = express();
+var server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+server.listen(3000);
+
+// Reconnaissance de la partie Client
+app.use(express.static(path.join(__dirname, "../Client")));
+
+//Lancement fichier index.html lors de l'arrivée sur localhost:3000
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../Client", "index.html"));
+});
 
 // Quand un client se connecte, le serveur lui envoie un message
 // Quand un client se connecte, on le note dans la console
-server.on("connection", function(socket) {
+io.on("connection", function(socket) {
   console.log("un client s'est connecté");
-  //permet l'envoie du message, 'message' = le type, 'Vous ...' = le contenu
   socket.emit("connection_ok", "Vous êtes bien connecté !");
   socket.on("login", function(pseudo) {
     // Lors du login, ajoute le pseudo du joueur + socketId
     var joueur = user_management.addJoueur(pseudo, socket.id);
+    // console.log(joueur.listeAttente);
 
-    // Test affichage
-    console.log("Joueur 1 :" + JSON.stringify(joueur.newJoueur));
-    console.log("Liste Joueur Connectés :" + JSON.stringify(joueur.joueurCo));
-
-    socket.emit("ready", pseudo + " ready pour attendre !");
+    if (joueur.listeAttente.length < 2) {
+      // Peut pas jouer car solo, donc il doit attendre
+      socket.emit("not ready", "on attend un 2ème joueur");
+    } else {
+      socket.emit("ready", pseudo + " ready pour jouer !");
+    }
   });
   socket.on("disconnect", function() {
-    console.log("user " + socket.id + " disconnected");
+    // console.log("user " + socket.id + " disconnected");
+    user_management.PlayerDisconnected(socket.id);
   });
 });
--- Serveur/user_management.js
+++ Serveur/user_management.js
@@ -1,3 +1,5 @@
+// <----------------- Variable Globales ----------------->
+
 let listJoueur = [
   { pseudo: "Jean", nbVictoire: 12 },
   { pseudo: "Ali", nbVictoire: 5 },
@@ -5,7 +7,9 @@ let listJoueur = [
 ];
 
 // Liste de joueurs connectés l'app
-let joueursCo = [];
+let ListeAttentejoueurs = [];
+
+// <----------------- Classes ----------------->
 
 class Joueur {
   constructor(pseudo) {
@@ -14,34 +18,46 @@ class Joueur {
   }
 }
 
+// <----------------- Fonctions ----------------->
 function PlayerConnected(id, pseudo) {
-  //   console.log(
-  //     "Add a new player in PlayerConnected list : " + id + " pseudo : " + pseudo
-  //   );
-  joueursCo.push({ id, pseudo });
+  ListeAttentejoueurs.push({ id, pseudo });
 }
 
-// Future fonction de suppression d'un joueur dans liste joueursCo
-function PlayerDisconnected(id, pseudo) {
-  console.log("Remove the player X from PlayerConnected list : " + id);
+// Future fonction de suppression d'un joueur dans liste ListeAttentejoueurs
+function PlayerDisconnected(id) {
+  ListeAttentejoueurs = ListeAttentejoueurs.filter(function(el) {
+    return el.id != id;
+  });
+
+  console.log(ListeAttentejoueurs);
 }
 
-module.exports = {
-  addJoueur: function(pseudo, id) {
-    // console.log("Pseudo envoyé : " + pseudo);
-    // console.log(list.find(joueur => joueur.pseudo === pseudo));
+// Fonction aléatoire de choix de couleur pour le joueur
+// 0 = Noir | 1 = Blanc
+function ChooseColorGame() {
+  return Math.floor(Math.random() * Math.floor(2));
+}
 
-    // Test de présence dans notre "BDD"
-    if ((joueur = listJoueur.find(joueur => joueur.pseudo === pseudo))) {
-      console.log(joueur.pseudo + " est présent.");
-      var newJoueur = joueur;
-    } else {
-      // Création d'un nouveau joueur si non présent
-      console.log("Non présent.");
-      var newJoueur = new Joueur(pseudo);
-    }
-    //Ajoute le joueur à la liste + retourne un objet Joueur et l'état de la liste
-    PlayerConnected(id, newJoueur.pseudo);
-    return { newJoueur: newJoueur, joueurCo: joueursCo };
+function addJoueur(pseudo, id) {
+  // Test de présence dans notre "BDD"
+  if ((joueur = listJoueur.find(joueur => joueur.pseudo === pseudo))) {
+    console.log(joueur.pseudo + " est présent dans 'bdd'.");
+    var newJoueur = joueur;
+  } else {
+    // Création d'un nouveau joueur si non présent
+    console.log("Non présent dans 'bdd'.");
+    var newJoueur = new Joueur(pseudo);
   }
-};
+  //Ajoute le joueur à la liste + retourne un objet Joueur et l'état de la liste
+  PlayerConnected(id, newJoueur.pseudo);
+  let hisColor = ChooseColorGame();
+  return {
+    newJoueur: newJoueur,
+    listeAttente: ListeAttentejoueurs,
+    color: hisColor
+  };
+}
+
+// <----------------- Exports ----------------->
+exports.addJoueur = addJoueur;
+exports.PlayerDisconnected = PlayerDisconnected;
