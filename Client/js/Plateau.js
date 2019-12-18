b267c3be5c2a5866651890121a7c112b4067e52a
Skiwa <haegman.julien@laposte.net>
2019-12-18 10:21:07 +0100

    Opti generation Plateau

--- Client/js/Plateau.js
+++ Client/js/Plateau.js
@@ -15,29 +15,12 @@ class Plateau {
                 this.plateau[i][j] = 0;
             }
         }
-        /* -- Rempli la grille avec les pions par défaut -- */
-        //blancs
-        for (let i = 0; i < 2; i++) {
-            //décale d'une position aux lignes paires
-            if (i % 2 == 0) {
-                j = 0;
-            }
-            else {
-                j = 1;
-            }
-            for (j; j < this.taille; j += 2) {
-                this.plateau[j][i] = new Pion('b'); //pose un pion blanc
-            }
-        }
-        //noirs
-        for (let i = this.taille - 1; i > this.taille - 3; i--) {
-            //décale d'une position aux lignes paires
-            if (i % 2 == 0)
-                j = 0;
-            else
-                j = 1;
-            for (j; j < this.taille; j += 2) {
-                this.plateau[j][i] = new Pion('n'); //pose un pion noir
+        //Ajoute les pions
+        for (let i = 0; i < this.taille; i++) {
+            for (let j = 0; j < this.taille; j++) {
+                if ((i < 2 || i > 7) && ((i + j) % 2 === 0)) {
+                    this.plateau[j][i] = new Pion(i < 2 ? 'b' : 'n');
+                }
             }
         }
     }
