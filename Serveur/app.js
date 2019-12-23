38c0645ecc48c18a7ad24c5ac01de589539eec00
Nicolas <nicolas.gauss.1@orange.fr>
2019-12-23 14:32:51 +0100

    Envoi mouvement + déplacement

--- Serveur/app.js
+++ Serveur/app.js
@@ -10,6 +10,7 @@ const io = require("socket.io")(server);
 
 var joueur;
 var listeAttente = [];
+var listeJeu = [];
 
 server.listen(3000);
 
@@ -40,28 +41,48 @@ io.on("connection", function(socket) {
       game_management.newPartie(listeAttente[0].joueur, listeAttente[1].joueur);
       var color1 = Math.random() >= 0.5 ? "noir" : "blanc";
       var color2 = color1 === "noir" ? "blanc" : "noir";
+      // listeJeu.push({JSockId:listeAttente[0].id, J:listeAttente[0].joueur},{JSockId:listeAttente[1].id, J:listeAttente[1].joueur});
       io.to(`${listeAttente[0].id}`).emit(
         "ready",
         JSON.stringify({
-          pseudo: listeAttente[1].joueur.pseudo,
-          color: color1
+          adversaire: listeAttente[1].joueur.pseudo,
+          yourColor: color1
         })
       );
       io.to(`${listeAttente[1].id}`).emit(
         "ready",
         JSON.stringify({
-          pseudo: listeAttente[0].joueur.pseudo,
-          color: color2
+          adversaire: listeAttente[0].joueur.pseudo,
+          yourColor: color2
         })
       );
       listeAttente.splice(0, 2);
     }
   }, 5000);
 
-  socket.on("message", function(message) {
-    // On récupère le pseudo de celui qui a cliqué dans les variables de session
-    console.log(socket.id + " me parle ! Il me dit : " + message);
+  socket.on("deplacement-joueur-envoi", function(move) {
+    console.log(socket.id + " move : " + JSON.stringify(move));
+    // console.log("inverse old x = ", 9 - move.anciennePosition.x);
+    // console.log("inverse old y = ", 9 - move.anciennePosition.y);
+    // console.log("inverse new x = ", 9 - move.nouvellePosition.x);
+    // console.log("inverse new y = ", 9 - move.nouvellePosition.y);
+    var inverseMove = {
+      anciennePosition: {
+        x: 9 - move.anciennePosition.x,
+        y: 9 - move.anciennePosition.y
+      },
+      nouvellePosition: {
+        x: 9 - move.nouvellePosition.x,
+        y: 9 - move.nouvellePosition.y
+      }
+    };
+    socket.broadcast.emit("deplacement-joueur-reception", inverseMove);
   });
+
+  // socket.on("message", function(message) {
+  //   // On récupère le pseudo de celui qui a cliqué dans les variables de session
+  //   console.log(socket.id + " me parle ! Il me dit : " + message);
+  // });
   socket.on("disconnect", function() {
     // console.log("user " + socket.id + " disconnected");
     user_management.PlayerDisconnected(socket.id);
