c7fd9ef9b61944b1ffa4a21ec887d89da684e24a
Skiwa <haegman.julien@laposte.net>
2019-12-26 23:47:45 +0100

    Suppression de fichiers useless et affichage de l'ip du serveur

--- Serveur/app.js
+++ Serveur/app.js
@@ -1,25 +1,21 @@
-// Imports modules
-var user_management = require("./user_management");
-var game_management = require("./game_management");
-
-var path = require("path");
+//Dépendances Node/npm
 var express = require("express");
 var app = express();
 var server = require("http").Server(app);
 const io = require("socket.io")(server);
+var os = require( 'os' );
 
+//Dépendances locales
+var user_management = require("./user_management");
+var game_management = require("./game_management");
+
+
+const portServeur = 3000;
 var listeAttente = [];
 
-server.listen(3000);
-
-// Reconnaissance de la partie Client
-app.use(express.static(path.join(__dirname, "../Client/www")));
-
-//Lancement fichier index.html lors de l'arrivée sur localhost:3000
-//../Client/www/vues
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../Client/www/", "index.html"));
-});
+//Lance le serveur
+server.listen(portServeur);
+console.log("Serveur lancé sur les addresses : " + getAddressesIp());
 
 // Quand un client se connecte au WebSocket, le serveur lui envoie un message
 io.on("connection", function(socket) {
@@ -59,8 +55,6 @@ io.on("connection", function(socket) {
   }, 5000);
 
   socket.on("deplacement-joueur-envoi", function(move) {
-    // console.log(socket.id + " move : " + JSON.stringify(move));
-
     // Appel game module pour inversion des déplacements
     var inverseMove = game_management.inverseDeplacement(move);
     socket.broadcast.emit("deplacement-joueur-reception", inverseMove);
@@ -72,7 +66,28 @@ io.on("connection", function(socket) {
   });
 
   socket.on("disconnect", function() {
-    // console.log("user " + socket.id + " disconnected");
     user_management.PlayerDisconnected(socket.id);
   });
 });
+
+
+
+/**
+ * TODO : A isoler dans un fichier ? 
+ * Récupère les addresses du serveur. 
+ * Utile pour les logs et savoir à quelle addresse se connecter
+ */
+function getAddressesIp(){  
+  var networkInterfaces = Object.values(os.networkInterfaces())
+      .reduce((r,a)=>{
+          r = r.concat(a)
+          return r;
+      }, [])
+      .filter(({family, address}) => {
+          return family.toLowerCase().indexOf('v4') >= 0 &&
+              address !== '127.0.0.1'
+      })
+      .map(({address}) => (address+":"+portServeur));
+  var ipAddresses = networkInterfaces.join(', ');
+  return(ipAddresses);
+}
