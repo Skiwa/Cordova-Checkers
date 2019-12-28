3a9cf934e3fbe69bccf7b48af722d13f03fa3d01
Liword38 <matnico@laposte.net>
2019-12-28 21:18:29 +0100

    Add: ecran d'attente d'adversaire + clean

--- Client/www/js/Login.js
+++ Client/www/js/Login.js
@@ -1,28 +1,35 @@
 class Login {
     //Crée un nouveau formulaire de connexion
     constructor() {
-        console.log("Affichage du formulaire de connexion");
+        this.divForm = document.createElement("div");
+        this.divForm.id = "popup-fond";
         //Affiche le formulaire de connexion
         this.afficherForm();
     }
     //Crée un formulaire et l'ajoute au DOM
     afficherForm() {
-        let divForm = document.createElement("div");
-        divForm.className = "form-conteneur";
-        divForm.id = "form-conteneur";
-        divForm.innerHTML = `
-        <form class="form-popup" id="form-popup">
+        this.divForm.innerHTML = `
+        <div class="popup-conteneur" >
+        <form id="theForm">
         <h1>Se connecter</h1>
         <label for="pseudo">Pseudo</label>
         <input type="text" placeholder="Votre pseudo" name="pseudo" required>
         <label for="password">Password</label>
         <input type="text" placeholder="Votre password" name="password" >
         <button type"submit" id="submitBtn">Ok</button>
-
         </form>
+        </div>
       `;
         document.getElementById("main").innerHTML = "";
-        document.getElementById("main").appendChild(divForm);
+        document.getElementById("main").appendChild(this.divForm);
     }
-    ;
+    //Crée et affiche l'écran d'attente
+    attenteScreen() {
+        this.divForm.innerHTML = `
+      <div class="popup-conteneur">
+      <h1>En attente d'un adversaire</h1>
+      <img src="images/Loading100px.svg">
+      </div>
+      `;
+    }
 }
