f810caae097962b2c7c91424eb708e38b411dda3
Liword38 <matnico@laposte.net>
2019-12-29 22:03:59 +0100

    Add: Page de fin de partie avec REJOUER et SCORES

---
+++ Client/www/ts/fin_match.ts
@@ -0,0 +1,36 @@
+class fin_match {
+    divBackground = document.createElement("div");      //Elelement contenant la popup de fin de match 
+    pseudo:String;                                      //Pseudo du joueur qui a fini de jouer
+
+    constructor(pseudo, isVictoire) {
+        //Récupère le pseudo du joueur
+        this.pseudo = pseudo;
+        
+        //Définit le style de l'élément racine de la popup
+        this.divBackground.id = "popup-fond";
+
+        //Génère et affiche la popup de fin de partie
+        this.afficherPopupFinMatch(isVictoire);
+    }
+
+
+    //Génère et affiche la popup de fin de partie
+    afficherPopupFinMatch(isVictoire) {
+        this.divBackground.innerHTML = `
+        <div class="popup-conteneur">
+        <h1>Partie terminée </h1>
+        <h3>` + this.messageFinal(isVictoire) + `</h3>
+        <div class="btns-fin-jeu">
+        <button type="button" class="btn-fin-jeu" id="btn-fin-jeu-g">REJOUER</button>
+        <button type="button" class="btn-fin-jeu" id="btn-fin-jeu-d">SCORES</button>
+        </button>
+        </div>
+        `;
+        document.getElementById("main").appendChild(this.divBackground);
+    }
+
+    //Un message de fin customisé
+    messageFinal(isVictoire) {
+        return isVictoire ? "Vous avez gagné! :)" : "Vous avez perdu... :'(";
+    }
+}
