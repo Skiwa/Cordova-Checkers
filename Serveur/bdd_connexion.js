ade3c3be4fd880562a8c881ade5ca45ce9342e6e
unknown <bettinagr@hotmail.fr>
2019-12-29 10:51:39 +0100

    Connexion à la bdd

---
+++ Serveur/bdd_connexion.js
@@ -0,0 +1,26 @@
+// Importation de mongoose
+let mongoose = require('mongoose');
+
+// Serveur de la bdd
+const server = '127.0.0.1:27017'; 
+// Nom de la bdd
+const database = 'dame-db';      
+
+class Database {
+  constructor() {
+    this._connect()
+  }
+
+// Connexion à la base de données 
+_connect() {
+     mongoose.connect(`mongodb://${server}/${database}`)
+       .then(() => {
+         console.log('La connexion à la bdd a réussi.')
+       })
+       .catch(err => {
+         console.error('La connexion à la bdd a échoué.')
+       })
+  }
+}
+
+module.exports = new Database()
