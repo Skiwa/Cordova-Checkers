3b162e8c461496ebe327a7106c51fc00ec2c7266
unknown <bettinagr@hotmail.fr>
2019-12-29 10:52:30 +0100

    Collection Utilisateur

---
+++ Serveur/models/user.js
@@ -0,0 +1,23 @@
+//Collection contenant les utilisateurs
+//Connexion à la base de données
+var bdd_connexion = require("../bdd_connexion");
+
+//Require Mongoose
+var mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+
+//Definition d'un schéma pour modéliser les données (<=> à une collection)
+var userSchema = new Schema({
+    id: Number,
+    name: {type: String, required: true}, 
+    password: String,
+    nbVictoire: Number
+});
+
+// Création d'un modèle qui respecte le schéma définit précédemment 
+// Permettra d'insérer dans la base de données
+let Utilisateur = mongoose.model('Utilisateur', userSchema);
+
+Utilisateur.exports = Utilisateur;
